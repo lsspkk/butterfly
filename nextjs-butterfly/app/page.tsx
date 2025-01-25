747a7f0648a5d3e750a501af4a44fb16554f21fa
lsspkk <lsspkk@gmail.com>
2025-01-25 03:50:35 +0200

    Fixes

--- nextjs-butterfly/app/page.tsx
+++ nextjs-butterfly/app/page.tsx
@@ -1,9 +1,10 @@
 'use client'
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import * as PIXI from 'pixi.js'
 import KeyboardListener, { TouchListener } from './game/systems/KeyboardListener'
 import { Level } from './game/worlds/Level'
 import { flowerNames, leafNames } from './game/entities/Bush'
+import { initEngine } from './game/systems/AudioSystem'
 
 // initialize the pixi application
 // and make a full screen view
@@ -82,12 +83,34 @@ export default function Home() {
 
   return (
     <div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
+      <StartDialog />
       <main className='flex flex-col gap-8 row-start-2 items-center sm:items-start'>
         <div className='w-screen h-screen absolute top-0 left-0 z-[-1]'>
           <canvas ref={canvasRef} className='w-screen h-screen' />
         </div>
       </main>
       <footer className='row-start-3 flex gap-6 flex-wrap items-center justify-center'></footer>
+    </div>
+  )
+}
+
+export function StartDialog() {
+  const [showDialog, setShowDialog] = useState(true)
+  const start = () => {
+    initEngine(new window.AudioContext())
+    setShowDialog(false)
+  }
+  if (!showDialog) return null
+
+  return (
+    <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-gray-700 p-8 rounded-lg shadow-xl'>
+      <h1 className='text-3xl font-bold text-center'>Welcome to the game</h1>
+
+      <div className='flex w-full justify-center gap-4'>
+        <button autoFocus className='bg-blue-500 text-white px-4 py-2 rounded-lg shadow-md mt-4' onClick={start}>
+          Start
+        </button>
+      </div>
     </div>
   )
 }
